d7b1cf50fa581ce43bc8fca5a7dea57ba6cd294e
arthurshag <65594296+arthurshag@users.noreply.github.com>
2022-05-22 14:37:15 +0500

    update

--- db.js
+++ db.js
@@ -1,10 +1,29 @@
-const Pool = require('pg').Pool
+const pg = require('pg');
+
+
+const Pool = require('pg').Pool;
+
 const pool = new Pool({
-    user: "postgres",
-    password: 'root',
-    host: 'localhost',
-    port: 5432,
-    database: "postgres"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+const pool = new Pool({
+    user: "bbspwoeqmarcys",
+    password: 'f4605af6bfb1a83bc21f5a277a4e3794c81ca0535ce55e2f5e035cfc03325078',
+    host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
+    port: 5432,
+    database: "d1e11aqtbmri63",
+});
+// const pool = new Pool({
+//     user: "postgres",
+//     password: 'root',
+//     host: 'localhost',
+//     port: 5432,
+//     database: "postgres"
+// });
+
 
 module.exports = pool;
--- routes/card.routes.js
+++ routes/card.routes.js
@@ -5,6 +5,18 @@ const cardController = require("../controller/card.controller");
 router.post("/card", cardController.createCard);
 // router.get("/card/:id", cardController.getOneUser);
 router.get("/cards", cardController.getCards);
+router.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
 // router.delete("/user/:id", cardController.deleteUser);
 // router.put("/user/:id", cardController.updateUser);
 
