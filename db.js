ddc32e67cec4edc56fa80165f148edc93f8f5d58
arthurshag <65594296+arthurshag@users.noreply.github.com>
2022-05-22 14:41:39 +0500

    up

--- db.js
+++ db.js
@@ -10,13 +10,14 @@ const pool = new Pool({
     }
 });
 
-const pool = new Pool({
-    user: "bbspwoeqmarcys",
-    password: 'f4605af6bfb1a83bc21f5a277a4e3794c81ca0535ce55e2f5e035cfc03325078',
-    host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    database: "d1e11aqtbmri63",
-});
+
+// const pool = new Pool({
+//     user: "bbspwoeqmarcys",
+//     password: 'f4605af6bfb1a83bc21f5a277a4e3794c81ca0535ce55e2f5e035cfc03325078',
+//     host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
+//     port: 5432,
+//     database: "d1e11aqtbmri63",
+// });
 // const pool = new Pool({
 //     user: "postgres",
 //     password: 'root',
